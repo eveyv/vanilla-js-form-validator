59354797733765128de1d601a6d12db1fa559f9c
Everett Yeaw <evyeaw@gmail.com>
2020-03-31 15:54:14 -0400

    writing the javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.classname = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+ if (re.test(input.value.trim())) {
+   showSuccess(input);
+ } else {
+   showError(input, 'Email is not valid');
+ }
+}
+
+function checkRequired(inputArray) {
+  inputArray.forEach(function(input) {
+    if(input.value.trim() === '') {
+      showError(input, 'is required');
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+})
