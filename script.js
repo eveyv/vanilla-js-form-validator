a8503ffe146dddc895a0c49053754a8b297e8622
Everett Yeaw <evyeaw@gmail.com>
2020-04-01 17:41:44 -0400

    changed global var name from password2 to confimration; refactored check email function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const confirmation = document.getElementById('confirmation');
 
 function showError(input, message) {
   const formControl = input.parentElement;
@@ -18,11 +18,11 @@ function showSuccess(input) {
 
 function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- if (re.test(input.value.trim())) {
-   showSuccess(input);
- } else {
-   showError(input, 'Email is not valid');
- }
+  if(re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
 function checkRequired(inputArray) {
@@ -58,7 +58,8 @@ function getFieldName(input) {
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  checkRequired([username, email, password, password2])
+  checkRequired([username, email, password, confirmation])
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
+  checkEmail(email)
 })
